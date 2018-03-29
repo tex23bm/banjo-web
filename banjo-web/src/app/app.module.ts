177ffe61dedd45c45bd8d8506c7b7cad3764cfd2
William Meehan <bill.meehan@gmail.com>
2018-03-28 21:04:32 -0500

    first attempt at routing, it does work with the dashboard

--- banjo-web/src/app/app.module.ts
+++ banjo-web/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
