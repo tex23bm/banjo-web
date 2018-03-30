29f2eda104ec40cb16687e001b4dc7bbf1f2ad30
William Meehan <bill.meehan@gmail.com>
2018-03-30 00:29:28 -0500

    fleshing otu some routes adding some styling and information

--- banjo-web/src/app/app-routing.module.ts
+++ banjo-web/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VenueComponent } from './venue/venue.component';
+import { StoryComponent } from './story/story.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'our-story', component: StoryComponent },
+  { path: 'venue', component: VenueComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
--- banjo-web/src/app/app.module.ts
+++ banjo-web/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VenueComponent } from './venue/venue.component';
+import { StoryComponent } from './story/story.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    VenueComponent,
+    StoryComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
