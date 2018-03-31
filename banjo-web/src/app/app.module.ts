14f6ef83524aea316fe8ae31f327ec97cb823c1a
William Meehan <bill.meehan@gmail.com>
2018-03-31 14:21:53 -0500

    playing with routing

--- banjo-web/src/app/app.module.ts
+++ banjo-web/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { VenueComponent } from './venue/venue.component';
 import { StoryComponent } from './story/story.component';
@@ -20,6 +21,7 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
   ],
   providers: [],
