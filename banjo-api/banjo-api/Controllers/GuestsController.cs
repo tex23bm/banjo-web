57aaeb98ab10b573762633f98f5be6be9489b4d2
William Meehan <bill.meehan@gmail.com>
2018-04-09 21:16:30 -0500

    initial commit, create solution, play with configuration

---
+++ banjo-api/banjo-api/Controllers/GuestsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using banjo_api.Configurations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace banjo_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Guests")]
+    public class GuestsController : Controller
+    {
+        public GuestsController(IConfiguration configuration, IOptions<CustomConfigurations> customOptions)
+        {
+            var something = configuration.GetConnectionString("something");
+        }
+
+        [Route("")]
+        [HttpGet]
+        public async Task<IActionResult> GetGuests()
+        {
+            var guests = new
+            {
+                name = "something",
+                value = "value"
+            };
+
+            return Ok(guests);
+        }
+    }
+}
---
+++ banjo-api/banjo-api/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using banjo_api.Configurations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace banjo_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddOptions();
+
+            var section = Configuration.GetSection("CustomConfigSection");
+            services.Configure<CustomConfigurations>(section);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
