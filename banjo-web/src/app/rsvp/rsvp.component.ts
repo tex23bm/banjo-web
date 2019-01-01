87784eab2e01b3037caad7e758aa64129d599fcb
William Meehan <bill.meehan@gmail.com>
2019-01-01 16:15:24 -0600

    adding ui to search for and update guests, still WIP

--- banjo-web/src/app/rsvp/rsvp.component.ts
+++ banjo-web/src/app/rsvp/rsvp.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GuestService } from '../services/guest.service';
+import { GuestSearchQueryModel } from '../data/guest-search-query-model';
+import { queryRefresh } from '@angular/core/src/render3/query';
+import { GuestResponseModel } from '../data/guest-response-model';
 
 @Component({
   selector: 'app-rsvp',
@@ -8,11 +11,73 @@ import { GuestService } from '../services/guest.service';
 })
 export class RsvpComponent implements OnInit {
 
+  guestQuery: GuestSearchQueryModel = {
+    LastName: '',
+    ZipCode: ''
+  };
+
+  possibleMatches: GuestResponseModel[] = null;
+  matchedResult: GuestResponseModel = null;
+  guestValidationMessage: String = null;
+
+
   constructor(
     private guestService: GuestService
   ) { }
 
   ngOnInit() {
+  }
+
+  searchForInvitation(query: GuestSearchQueryModel) {
+    this.guestService.searchForGuests(query)
+      .subscribe(matches => this.possibleMatches = matches);
+
+    if (this.possibleMatches != null && this.possibleMatches.length === 1) {
+      this.setMatchedResult(this.possibleMatches[0]);
+    }
+
+  }
+
+  clearSearchResults() {
+    this.possibleMatches = null;
+  }
+
+  setMatchedResult(match: GuestResponseModel) {
+    this.possibleMatches = null;
+    this.matchedResult = match;
+  }
+
+  clearMatchedResult() {
+    this.matchedResult = null;
+  }
+
+  declineInvitation(guest: GuestResponseModel) {
+    guest.confirmedGuests = 0;
+
+    this.confirmInvitation(guest);
+  }
+
+  validateGuest(guest: GuestResponseModel) {
+    if (guest.confirmedGuests == null || guest.confirmedGuests > guest.totalGuestsAllowed) {
+      this.guestValidationMessage = 'Sorry, but you have exceeded the total number of allowed guests ('
+      + this.matchedResult.totalGuestsAllowed + ').';
+
+      return false;
+    }
+
+    this.guestValidationMessage = null;
+    return true;
+  }
+
+  confirmInvitation(guest: GuestResponseModel) {
+
+    if (!this.validateGuest(guest)) {
+      return;
+    }
+
+    this.guestService.updateGuest(guest);
+
+    this.clearMatchedResult();
   }
 
 }
--- banjo-web/src/app/services/guest.service.ts
+++ banjo-web/src/app/services/guest.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import { of } from 'rxjs/observable/of';
 import { environment } from '../../environments/environment';
+import { GuestSearchQueryModel } from '../data/guest-search-query-model';
+import { GuestResponseModel } from '../data/guest-response-model';
 
 @Injectable()
 export class GuestService {
@@ -11,6 +15,49 @@ export class GuestService {
     private http: HttpClient,
   ) {
     console.log(this.baseUrl);
+  }
+
+  searchForGuests(query: GuestSearchQueryModel): Observable<GuestResponseModel[]> {
+    const guests: GuestResponseModel[] = [
+      { id: 1, name: 'Bill', partner: 'Onja', lastName: 'Meehan', address: '2211 Grapevine ln'
+        , zipcode: '75007', totalGuestsAllowed: 2, confirmedGuests: undefined, dateModified: new Date('12/30/2018') },
+      { id: 1, name: 'Manny', partner: null, lastName: 'Guzman', address: '42 Maynard Rd'
+        , zipcode: '75007', totalGuestsAllowed: 1, confirmedGuests: undefined, dateModified: new Date('12/31/2018') }
+    ];
+
+    const url = this.buildGuestSearchUrl(query);
+
+    console.log(url);
+    // this.http.get<GuestResponseModel[]>(url);
+
+    return of(guests);
+  }
+
+  updateGuest(guest: GuestResponseModel) {
+    alert('Saved guest: ' + guest.name + ' with ' + guest.confirmedGuests + ' guests confirmed' );
+  }
+
+  private buildGuestSearchUrl(query: GuestSearchQueryModel): String {
+    let url = this.baseUrl;
+
+    if (query.ZipCode !== '' || query.LastName !== '') {
+      url += '?';
+
+      if (query.ZipCode !== '') {
+        url += `zipCode=${query.ZipCode}`;
+
+        if (query.LastName !== '') {
+          url += '&';
+        }
+      }
+
+      if (query.LastName !== '') {
+        url += `lastName=${query.LastName}`;
+      }
+
+    }
+
+    return url;
   }
 
 }
